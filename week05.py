3f2ee2c55ee0a8a375bcca80497e4e48cad6ba85
unknown <202230212@daelim.ac.kr>
2026-04-02 16:20:18 +0900

    week05 amusement park app v0.3

--- week05.py
+++ week05.py
@@ -1,11 +1,12 @@
-#import ticket
+import ticket
 
-humans = int(input("몇 명?"))
+humans = int(input("몇 명? "))
 ages = list()
 age = 0
 
 for i in range(humans):
-    age = int(input("나이 ?"))
+    age = int(input("나이 ? "))
     ages.append(age)
 
-print(ages)
+print(f"총 요금은 {ticket.entrance_fee(ages)}원 입니다.")
+# print(ages)
