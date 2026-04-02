f50d4441345b8277fd2dbd0261e4eafb76555f90
unknown <202230212@daelim.ac.kr>
2026-04-02 15:52:35 +0900

    week05 amusement park app v0.2

--- ticket.py
+++ ticket.py
@@ -1,2 +1,18 @@
-def entrance_free():
-    pass
+def entrance_fee(ages) -> int:
+    """
+    Calculates the admission fee for the amusement park.
+    Parameters :
+        ages : List of Ages
+    Returns:
+        int: The total calculated entry fee.
+    """
+    kid, adult, senior = 5000, 10000, 7000
+    total_free = 0
+    for age in ages:
+        if age >= 65:
+            total_free = total_free + senior
+        elif age >= 19:
+            total_free = total_free + adult
+        else:
+            total_free = total_free + kid
+    return total_free
