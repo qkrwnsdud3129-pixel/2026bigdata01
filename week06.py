9a41b252e61ccc02b664d77b92319cef8046abd8
박준영 <qkrwnsdud3129@gmail.com>
2026-04-09 15:53:00 +0900

    week06 numpy v0.1

--- week06.py
+++ week06.py
@@ -1,33 +1,25 @@
+#배열의구조와정보를파악할때사용합니다.(arr가배열일때)
+#arr.shape:배열의크기(예:(3,4)는3행4열)
+#arr.ndim:차원수(1차원,2차원등)
+#arr.dtype:요소의데이터타입(int64,float64등)
+#arr.size:전체요소의개수
+
+#1.메모리관련속성
+#arr.itemsize:각요소(Element)하나가차지하는바이트(byte)수입니다.
+#arr.nbytes:배열전체가차지하는총바이트수입니다.(itemsize*size와같습니다.)
+#arr.strides:다음요소나다음행으로이동하기위해메모리상에서몇바이트를건너뛰어야하는지나타내는튜플입니다.
+#2.데이터표현속성
+#arr.T:배열의전치(Transpose)된결과입니다.(행과열을바꿈)
+#arr.flat:다차원배열을1차원으로펼친상태의반복자(Iterator)입니다.
+#arr.real/arr.imag:복소수배열일경우각각실수부와허수부를반환합니다.
+#3.기타상태확인
+#arr.flags:배열의메모리배치방식(C-style인지Fortran-style인지),쓰기가능여부등을보여줍니다.
+#arr.base:이배열이다른배열의뷰(View)인지,아니면독자적인데이터를가진객체인지확인해줍니다. (메모리 절약을
+
 import numpy as np
 
-# 리스트로 생성 np.array([1, 2, 3])
-# 연속 값 생성 (0부터 10 미만까지 2씩 증가) np.arange(0, 10, 2)
-# 특수 배열 np.zeros((2, 3))
-# 0으로 채워진 2행 3열 배열 np.ones((2, 3))
-# 1로 채워진 2행 3열 배열 np.full((2, 2), 7)    # 모든 요소를 7로 채움
-# 난수 생성 (0~1 사이 랜덤 값) np.random.rand(2, 3)
-
-import numpy as np
-
-l1 = [1, 2, 3]
-array01 = np.array(l1)
-print(l1)
+#array01 = np.random.random((2,3,3))
+array01 = np.random.random((4,2))
 print(array01)
-
-array02 = np.arange(0, 10, 2)
-print(array02)
-
-array03 = np.zeros((2,3))
-print(array03)
-
-array04 = np.ones((2,3))
-print(array04)
-
-array05 = np.full((2,3), -1)
-print(array05)
-
-array06 = np.random.rand(2,3)
-print(array06)
-
-array07 = np.linspace(0,10,3)
-print(array07)
+print(array01.shape, array01.dtype, array01.ndim, array01.size)
+print(array01.T)
